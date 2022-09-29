cd780a2573f7ba43e3f0a4c2f87b38ecb9989753
Jase Languasco <jase@janestreetdigital.com>
2022-09-28 21:04:34 -0400

    Forgot to build

--- dist/main.umd.cjs
+++ dist/main.umd.cjs
@@ -1,12 +1,12 @@
-(function(o){typeof define=="function"&&define.amd?define(o):o()})(function(){"use strict";const o="";function l(n){const r=new URLSearchParams({pathname:window.location.pathname,siteKey:n});fetch(`https://responserocket.test/api/page?${r}`).then(t=>t.json()).then(t=>{const e=document.getElementsByClassName("response-rocket-count");for(let s=0;s<e.length;s++)e[s].innerHTML=t.reactions}).catch(t=>console.error(t))}const d=`
+(function(s){typeof define=="function"&&define.amd?define(s):s()})(function(){"use strict";const s="";function l(o){const e=new URLSearchParams({pathname:window.location.pathname,siteKey:o});fetch(`https://responserocket.test/api/page?${e}`).then(n=>n.json()).then(n=>{const r=document.getElementsByClassName("response-rocket-count");for(let t=0;t<r.length;t++)r[t].innerHTML=n.reactions}).catch(n=>console.error(n))}const d=`
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="rr-w-6 rr-h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
-</svg>`,m="https://responserocket.test/api/increment",c=document.querySelector("script[site_key]"),a=c.getAttribute("site_key"),u=document.body.classList.contains("darkMode")||c.getAttribute("darkMode");document.querySelector(".response-rocket").innerHTML=`
-  <button
-    type="button"
-    class="response-rocket-button rr-button-reset rr-rounded-full rr-p-4 rr-shadow rr-flex rr-items-center ${u?"rr-bg-white rr-text-black":"rr-bg-black rr-text-white"}"
-  >
-    ${d}
-    <span class="response-rocket-count">0</span>
-  </button>
-`;const i=document.getElementsByClassName("response-rocket-button");for(let n=0;n<i.length;n++)i[n].addEventListener("click",function(){const{hash:r,pathname:t}=window.location;fetch(m,{method:"POST",headers:{"Content-Type":"application/json"},mode:"cors",body:JSON.stringify({siteKey:a,hash:r,pathname:t})}).then(e=>e.json()).then(e=>this.getElementsByTagName("span")[0].innerHTML=e.reactions).catch(e=>console.error(e))});l(a)});
+</svg>`,h="https://responserocket.test/api/increment",a=document.querySelector("script[site_key]"),i=a.getAttribute("site_key"),m=document.body.classList.contains("darkMode")||a.getAttribute("darkMode"),c=document.querySelector(".response-rocket");if(c.length){for(let e=0;e<c.length;e++)c[e].innerHTML=`
+        <button
+          type="button"
+          class="response-rocket-button rr-button-reset rr-rounded-full rr-p-4 rr-shadow rr-flex rr-items-center ${m?"rr-bg-white rr-text-black":"rr-bg-black rr-text-white"}"
+        >
+          ${d}
+          <span class="response-rocket-count">0</span>
+        </button>
+      `;const o=document.getElementsByClassName("response-rocket-button");for(let e=0;e<o.length;e++)o[e].addEventListener("click",function(){const{hash:n,pathname:r}=window.location;fetch(h,{method:"POST",headers:{"Content-Type":"application/json"},mode:"cors",body:JSON.stringify({siteKey:i,hash:n,pathname:r})}).then(t=>t.json()).then(t=>this.getElementsByTagName("span")[0].innerHTML=t.reactions).catch(t=>console.error(t))});l(i)}});
--- main.js
+++ main.js
@@ -11,43 +11,41 @@ const darkMode = document.body.classList.contains('darkMode') || me.getAttribute
 
 const responseRocketContainers = document.querySelector('.response-rocket');
 
-if (! responseRocketContainers.length) {
-  return;
+if (responseRocketContainers.length) {
+  for (let i = 0; i < responseRocketContainers.length; i++) {
+    responseRocketContainers[i].innerHTML = `
+        <button
+          type="button"
+          class="response-rocket-button rr-button-reset rr-rounded-full rr-p-4 rr-shadow rr-flex rr-items-center ${darkMode ? 'rr-bg-white rr-text-black' : 'rr-bg-black rr-text-white'}"
+        >
+          ${rocketSvg}
+          <span class="response-rocket-count">0</span>
+        </button>
+      `;
+  }
+
+  const buttons = document.getElementsByClassName('response-rocket-button');
+
+  for (let i = 0; i < buttons.length; i ++) {
+    buttons[i].addEventListener('click', function () {
+      const { hash, pathname } = window.location;
+      fetch(incrementUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        mode: 'cors',
+        body: JSON.stringify({
+          siteKey,
+          hash,
+          pathname,
+        })
+      })
+      .then((response) => response.json())
+      .then((data) => this.getElementsByTagName('span')[0].innerHTML = data.reactions)
+      .catch((err) => console.error(err));
+    });
+  }
+
+  getPage(siteKey);
 }
-
-for (let i = 0; i < responseRocketContainers.length; i++) {
-  responseRocketContainers[i].innerHTML = `
-      <button
-        type="button"
-        class="response-rocket-button rr-button-reset rr-rounded-full rr-p-4 rr-shadow rr-flex rr-items-center ${darkMode ? 'rr-bg-white rr-text-black' : 'rr-bg-black rr-text-white'}"
-      >
-        ${rocketSvg}
-        <span class="response-rocket-count">0</span>
-      </button>
-    `;
-}
-
-const buttons = document.getElementsByClassName('response-rocket-button');
-
-for (let i = 0; i < buttons.length; i ++) {
-  buttons[i].addEventListener('click', function () {
-    const { hash, pathname } = window.location;
-		fetch(incrementUrl, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-      mode: 'cors',
-			body: JSON.stringify({
-				siteKey,
-				hash,
-				pathname,
-			})
-		})
-    .then((response) => response.json())
-		.then((data) => this.getElementsByTagName('span')[0].innerHTML = data.reactions)
-    .catch((err) => console.error(err));
-  });
-}
-
-getPage(siteKey);
